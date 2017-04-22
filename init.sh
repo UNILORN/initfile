815ce11f8af4aec7fef066bef8295d8df5244fe9
UNILORN <UNILORN@users.noreply.github.com>
2017-04-23 00:03:25 +0900

    少し変更

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-
-echo "Initialize Program on UNILORN!!"
-
 echo "##########################################################"
+echo "Initialize Program on UNILORN!!"
+echo "##########################################################"
+echo ""
+echo "####################### apt update #######################"
 sudo apt update
+sudo apt -y install curl wget
 sudo apt -y install vim
 sudo apt -y install php7.0 php7.0-mysql php7.0-dev php7.0-fpm php7.0-mbstring
 sudo apt -y install mysql-server mysql-client
@@ -13,9 +15,9 @@ sudo apt -y install git
 sudo apt -y install libapache2-mod-php7.0
 sudo apt -y install npm
 
-sudo apt -y install curl 
-sudo apt -y install fish
+sudo curl -o /usr/local/bin/jq -L https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 && sudo chmod +x /usr/local/bin/jq
 
+# sudo apt -y install fish
 # curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
 
 echo "################### composer install #####################"
@@ -23,8 +25,7 @@ curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 composer config -g repositories.packagist composer https://packagist.jp
 
-
-
-sudo apt -y upgrade
+# 任意
+# sudo apt -y upgrade
 
 
