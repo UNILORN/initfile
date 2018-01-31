e4925692c52e1ad355b0239084022825de1f64bc
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2018-01-31 19:01:00 +0900

    add

--- init.sh
+++ init.sh
@@ -15,13 +15,37 @@ sudo add-apt-repository ppa:ondrej/php
 sudo apt update
 sudo apt -y install php7.2 php7.2-xml php7.2-mysql php7.2-dev php7.2-fpm php7.2-mbstring
 
+# MySQL Install
 sudo apt -y install mysql-server mysql-client
-sudo apt -y install apache2
-sudo apt -y install ruby
-sudo apt -y install git
-sudo apt -y install libapache2-mod-php7.2
-sudo apt -y install npm
 
+# Apache Install
+sudo apt -y install apache2
+
+# Ruby Install
+sudo apt -y install ruby
+
+# git Install
+sudo apt -y install git
+
+sudo apt -y install libapache2-mod-php7.2
+
+# Node Install
+sudo apt -y install npm
+sudo npm install -g n
+sudo n latest
+
+# Python Install
+sudo apt -y install build-essential libncursesw5-dev libgdbm-dev libc6-dev zlib1g-dev libsqlite3-dev tk-dev libssl-dev openssl libbz2-dev libreadline-dev
+
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.profile
+source ~/.profile
+sudo pyenv install 3.6.0
+sudo pyenv global 3.6.0
+
+# jq Install
 sudo curl -o /usr/local/bin/jq -L https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 && sudo chmod +x /usr/local/bin/jq
 
 # sudo apt -y install fish
